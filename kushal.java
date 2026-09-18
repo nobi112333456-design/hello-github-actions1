9706faecfd6496dfe5870c65f462fd416a13083c
Nobita112333456 <nobi112333456@gmail.com>
2026-09-18 10:20:38 +0530

    Update kushal.java

--- kushal.java
+++ kushal.java
@@ -2,6 +2,7 @@
 class Codechef { 
     public static void main(String[] args) { 
         // Initialize a Scanner object to read input from the user
+        System.out.println("I am Kushal");
        
     }
 }
